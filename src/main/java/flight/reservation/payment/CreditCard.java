430581af1b5d113d805ff6a6771bd2b9a35c1d5e
Karan Bhatt <kbbhatt04@gmail.com>
2024-02-23 15:32:31 +0530

    Strategy Pattern for Payment

--- src/main/java/flight/reservation/payment/CreditCard.java
+++ src/main/java/flight/reservation/payment/CreditCard.java
@@ -5,7 +5,7 @@ import java.util.Date;
 /**
  * Dummy credit card class.
  */
-public class CreditCard {
+public class CreditCard implements PaymentStrategy {
     private double amount;
     private String number;
     private Date date;
@@ -35,5 +35,29 @@ public class CreditCard {
     public void setValid() {
         // Dummy validation
         this.valid = number.length() > 0 && date.getTime() > System.currentTimeMillis() && !cvv.equals("000");
+    }
+
+    @Override
+    public boolean pay(double amount) {
+        return payWithCreditCard(this, amount);
+    }
+
+    private boolean cardIsPresentAndValid(CreditCard card) {
+        return card != null && card.isValid();
+    }
+
+    public boolean payWithCreditCard(CreditCard card, double amount) throws IllegalStateException {
+        if (cardIsPresentAndValid(card)) {
+            System.out.println("Paying " + amount + " using Credit Card.");
+            double remainingAmount = card.getAmount() - amount;
+            if (remainingAmount < 0) {
+                System.out.printf("Card limit reached - Balance: %f%n", remainingAmount);
+                throw new IllegalStateException("Card limit reached");
+            }
+            card.setAmount(remainingAmount);
+            return true;
+        } else {
+            return false;
+        }
     }
 }
---
+++ src/main/java/flight/reservation/payment/PaymentContext.java
@@ -0,0 +1,17 @@
+package flight.reservation.payment;
+
+public class PaymentContext {
+    private PaymentStrategy paymentStrategy;
+
+    public PaymentContext(PaymentStrategy paymentStrategy) {
+        this.paymentStrategy = paymentStrategy;
+    }
+
+    public void setPaymentStrategy(PaymentStrategy paymentStrategy) {
+        this.paymentStrategy = paymentStrategy;
+    }
+
+    public void pay(double amount) {
+        paymentStrategy.pay(amount);
+    }
+}
